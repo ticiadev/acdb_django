8b339987e3ebce936f0fdeb56d5bafd1059d81d7
Ticia Francisco <ticiafrancisco@gmail.com>
2021-12-29 23:13:24 -0800

    Update Personality plural

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ class Personality(models.Model):
     type = models.CharField(max_length=12, unique=True)
     gender = models.CharField(max_length=8)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "personalities"
+
 class Villager(models.Model):
     SUBTYPE_CHOICES = [("A", "A"), ("B", "B")]
     name = models.CharField(max_length=12, unique=True)
