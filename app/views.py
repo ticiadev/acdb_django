8611b6ff7cd6809a1fb57b2b5e89a44bfd6200ef
Ticia Francisco <ticiafrancisco@gmail.com>
2021-12-29 23:13:41 -0800

    Update views and endpoints

--- app/views.py
+++ app/views.py
@@ -6,25 +6,36 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from .models import Villager, Personality
 from .serializers import VillagerSerializer, PersonalitySerializer
 
 
-class VillagerList(APIView):
+# class VillagerList(APIView):
+#
+#     def get(self, request):
+#         villager1 = Villager.objects.all()
+#         serializer = VillagerSerializer(villager1, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         pass
+#
+#
+# class VillagerDetail(APIView):
+#
+#     def get(self, request, id):
+#         villager1 = Villager.objects.get(id=id)
+#         serializer = VillagerSerializer(villager1)
+#         # print(serializer)
+#         return Response(serializer.data)
 
-    def get(self, request):
-        villager1 = Villager.objects.all()
-        serializer = VillagerSerializer(villager1, many=True)
-        return Response(serializer.data)
+class VillagerViewSet(viewsets.ModelViewSet):
+    """Allow villagers to be shown or edited."""
+    queryset = Villager.objects.all()
+    serializer_class = VillagerSerializer
 
-    def post(self, request):
-        pass
-
-
-class VillagerDetail(APIView):
-
-    def get(self, request, id):
-        villager1 = Villager.objects.get(id=id)
-        serializer = VillagerSerializer(villager1)
-        # print(serializer)
-        return Response(serializer.data)
+class PersonalityViewSet(viewsets.ModelViewSet):
+    """Allow personalities to be shown or edited."""
+    queryset = Personality.objects.all()
+    serializer_class = PersonalitySerializer
