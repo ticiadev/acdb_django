73e305d0fea037339658e5ce9099eb6227cf48c4
Ticia Francisco <ticiafrancisco@gmail.com>
2021-12-30 19:12:09 -0800

    Add all tables

--- app/models.py
+++ app/models.py
@@ -12,6 +12,22 @@ class Personality(models.Model):
     class Meta:
         verbose_name_plural = "personalities"
 
+class Coffee(models.Model):
+    beans = models.CharField(max_length=20)
+    milk = models.CharField(max_length=8)
+    sugar = models.IntegerField()
+
+    def __str__(self):
+        return self.id
+
+class Game(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    console = models.CharField(max_length=12)
+    year_released = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
 class Villager(models.Model):
     SUBTYPE_CHOICES = [("A", "A"), ("B", "B")]
     name = models.CharField(max_length=12, unique=True)
@@ -27,6 +43,39 @@ class Villager(models.Model):
                                blank=True)
     # personality_id = models.ForeignKey(Personality,
     #                                    on_delete=models.CASCADE)
+    # coffee_id = models.ForeignKey(Coffee, on_delete=models.SET_NULL,
+    # null=True)
+    # game_id = models.ManyToManyField(Game)
+
+    def __str__(self):
+        return self.name
+
+class Amiibo(models.Model):
+    series = models.CharField(max_length=20, unique=True)
+    card_id = models.IntegerField()
+    # villager_id = models.ForeignKey(Villager, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+class House(models.Model):
+    wallpaper = models.CharField(max_length=16)
+    floor = models.CharField(max_length=16)
+    music = models.CharField(max_length=16, blank=True)
+    # game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    # villager_id = models.ForeignKey(Villager, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+class Furniture(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    buy_price = models.IntegerField()
+    sell_price = models.IntegerField()
+    style = models.CharField(max_length=20, blank=True)
+    on_surface = models.BooleanField(blank=True, null=True)
+    length = models.IntegerField(null=True)
+    width = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
