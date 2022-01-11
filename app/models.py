0b74f2b0fa689d9111490dba112c247a584e2359
Ticia Francisco <ticiafrancisco@gmail.com>
2022-01-10 22:25:08 -0800

    Update models.py to add foreign keys and relationships, add migration file

--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ class Personality(models.Model):
     class Meta:
         verbose_name_plural = "personalities"
 
+
 class Coffee(models.Model):
     beans = models.CharField(max_length=20)
     milk = models.CharField(max_length=8)
@@ -19,6 +20,7 @@ class Coffee(models.Model):
 
     def __str__(self):
         return self.id
+
 
 class Villager(models.Model):
     SUBTYPE_CHOICES = [("A", "A"), ("B", "B")]
@@ -33,41 +35,47 @@ class Villager(models.Model):
     is_sanrio = models.BooleanField(blank=True, null=True)
     subtype = models.CharField(max_length=1, choices=SUBTYPE_CHOICES,
                                blank=True)
-    # personality = models.ForeignKey(Personality,
-    #                                    on_delete=models.CASCADE)
-    # coffee_id = models.ForeignKey(Coffee, on_delete=models.SET_NULL,
-    # blank=True, null=True)
+    personality = models.ForeignKey(Personality,
+                                    on_delete=models.CASCADE)
+    coffee_id = models.ForeignKey(Coffee, on_delete=models.SET_NULL,
+                                  blank=True, null=True)
 
     def __str__(self):
         return self.name
+
 
 class Amiibo(models.Model):
     series = models.CharField(max_length=20, unique=True)
     card_id = models.IntegerField()
     acquired = models.BooleanField(blank=True, null=True)
-    # villager = models.OneToOneField(Villager)
+    villager = models.OneToOneField(Villager, on_delete=models.CASCADE,
+                                    blank=True, null=True)
 
     def __str__(self):
         return self.id
+
 
 class Game(models.Model):
     name = models.CharField(max_length=20, unique=True)
     console = models.CharField(max_length=12)
     year_released = models.IntegerField()
-    # villagers = models.ManyToManyField(Villager)
+    villagers = models.ManyToManyField(Villager)
 
     def __str__(self):
         return self.name
+
 
 class House(models.Model):
     wallpaper = models.CharField(max_length=16)
     floor = models.CharField(max_length=16)
     music = models.CharField(max_length=16, blank=True)
-    # game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    # villager = models.ForeignKey(Villager, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    villager = models.ForeignKey(Villager, on_delete=models.CASCADE,
+                                 blank=True, null=True)
 
     def __str__(self):
         return self.id
+
 
 class Furniture(models.Model):
     name = models.CharField(max_length=20, unique=True)
@@ -77,7 +85,7 @@ class Furniture(models.Model):
     on_surface = models.BooleanField(blank=True, null=True)
     length = models.IntegerField(blank=True, null=True)
     width = models.IntegerField(blank=True, null=True)
-    # house_id = models.ManytoManyField(House)
+    house_id = models.ManyToManyField(House)
 
     def __str__(self):
         return self.name
